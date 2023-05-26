cca5ca21051676a46f77e6da4c5908830090d01d
Yura <113457886+Yura-Platonov@users.noreply.github.com>
2023-05-26 23:53:37 +0300

    fixed bugs

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,32 +1,50 @@
-export function fetchBreeds() {
-  return fetch('https://api.thecatapi.com/v1/breeds')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to retrieve breeds list.');
-      }
-      return response.json();
-    })
-    .then(data => {
-      return data.map(breed => ({
-        id: breed.id,
-        name: breed.name
-      }));
-    });
-}
-export function fetchCatByBreed(breedId) {
-  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+const URL = 'https://api.thecatapi.com/v1/breeds';
+const IMAGE_URL = 'https://api.thecatapi.com/v1/images/search';
+const API_KEY =
+  'live_0IgZa7IH1fpaWqdplmMVv1LV38TxUjv021FhoSLmCmZ7mDHnKHSgRhj96Q9O9kZ4';
 
-  return fetch(url)
+function fetchBreeds() {
+  return fetch(URL, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  })
     .then(response => {
       if (!response.ok) {
-        throw new Error('Failed to fetch cat information.');
+        throw new Error('Произошла ошибка при выполнении запроса');
       }
       return response.json();
     })
     .then(data => {
-      return data;
+      return data.map(({ id, name }) => ({ id, name }));
     })
     .catch(error => {
+      console.log('Ошибка:', error.message);
       throw error;
     });
 }
+
+function fetchCatByBreed(breedId) {
+  const url = `${IMAGE_URL}?breed_ids=${breedId}`;
+
+  return fetch(url, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Произошла ошибка при выполнении запроса');
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data[0]; // Возвращаем первый объект из массива
+    })
+    .catch(error => {
+      console.log('Ошибка:', error.message);
+      throw error;
+    });
+}
+
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
-import SlimSelect from 'slim-select'
-import Notiflix from 'notiflix';
-
-
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
-// Вызов функции fetchBreeds() для получения массива пород
-fetchBreeds()
-  .then(breeds => {
-    // Используйте массив пород здесь, например, для заполнения элемента select
-    const selectElement = document.querySelector('.breed-select');
-    breeds.forEach(breed => {
-      const option = document.createElement('option');
-      option.value = breed.id;
-      option.textContent = breed.name;
-      selectElement.appendChild(option);
-    });
-  })
-  .catch(error => {
-    console.log(error);
-  });
+import Notiflix from 'notiflix';
 
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
+const error = document.querySelector('.error');
+const loader = document.querySelector('.loader');
+
+function showLoader() {
+  loader.style.display = 'block';
+  breedSelect.style.display = 'none';
+}
+
+function hideLoader() {
+  loader.style.display = 'none';
+  breedSelect.style.display = 'block';
+}
+
+function showError(message) {
+  Notiflix.Notify.Failure(message);
+}
+
+showLoader();
+
+fetchBreeds()
+  .then(breeds => {
+    const breedOptions = breeds.map(({ id, name }) => {
+      const option = document.createElement('option');
+      option.value = id;
+      option.textContent = name;
+      return option;
+    });
+    breedSelect.append(...breedOptions);
+    hideLoader();
+  })
+  .catch(error => {
+    console.log('Произошла ошибка:', error);
+    hideLoader();
+    showError('Oops! Something went wrong! Try reloading the page!');
+  });
 
 breedSelect.addEventListener('change', () => {
   const selectedBreedId = breedSelect.value;
@@ -29,47 +46,19 @@ breedSelect.addEventListener('change', () => {
   showLoader();
 
   fetchCatByBreed(selectedBreedId)
-    .then(data => {
-      hideLoader();
-      showCatInfo(data);
+    .then(catData => {
+      catInfo.innerHTML = `
+        <img src="${catData.url}" alt="Cat Image" width="700" height="500">
+        <h3>${catData.breeds[0].name}</h3>
+        <p><strong>Description:</strong> ${catData.breeds[0].description}</p>
+        <p><strong>Temperament:</strong> ${catData.breeds[0].temperament}</p>
+      `;
     })
     .catch(error => {
+      console.log('Произошла ошибка:', error);
+      showError('Oops! Something went wrong!');
+    })
+    .finally(() => {
       hideLoader();
-      showError();
-      console.log(error);
     });
 });
-
-function showLoader() {
-  const loader = document.querySelector('.loader');
-  loader.style.display = 'block';
-}
-
-function hideLoader() {
-  const loader = document.querySelector('.loader');
-  loader.style.display = 'none';
-}
-
-function showError() {
-  const error = document.querySelector('.error');
-  error.style.display = 'block';
-}
-
-function hideError() {
-  const error = document.querySelector('.error');
-  error.style.display = 'none';
-}
-
-function showCatInfo(data) {
-    const breed = data.breeds[0];
-  
-    const catInfoHTML = `
-      <h2>Cat Information</h2>
-      <p><strong>Breed:</strong> ${breed.name}</p>
-      <p><strong>Description:</strong> ${breed.description}</p>
-      <p><strong>Temperament:</strong> ${breed.temperament}</p>
-      <img src="${data.url}" alt="Cat Image">
-    `;
-    catInfo.innerHTML = catInfoHTML;
-  }
-  
